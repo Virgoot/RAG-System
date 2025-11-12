e3dc74da78bdaad6d5510a9e11747b428e1ec4da
Virgoot <gsecond77@gmail.com>
2025-11-12 20:36:08 +0400

    last added changes with script

--- main.py
+++ main.py
@@ -1,42 +1,72 @@
 import faiss
+import psycopg2
+import numpy as np
+import ast
+import os
 
-from sentence_transformers import SentenceTransformer
+def parse_vector_str(vstr: str):
+    """Преобразует строку '[0.1, 0.2, 0.3]' в np.array(float32)."""
+    vlist = ast.literal_eval(vstr)  # безопаснее, чем eval()
+    return np.array(vlist, dtype=np.float32)
 
-# GET QUESTIONS AND ANSWERS / IN FUTURE NOT USING TEXT
-q = ["123", "222", "2222"]
-a = ["22", "12345", "-2222"]
+QUESTIONS_FILE = "questions.npy"
+ANSWERS_FILE = "answers.npy"
+
+if os.path.exists(QUESTIONS_FILE) and os.path.exists(ANSWERS_FILE):
+    questions_vectors = np.load(QUESTIONS_FILE)
+    answers_vectors = np.load(ANSWERS_FILE)
+else:
+    conn = psycopg2.connect(
+        host="ragdb-rag1.db-msk0.amvera.tech",
+        database="RAG",
+        user="rag",
+        password="rag"
+    )
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT q_vector FROM questions")
+        questions_vectors = np.array(
+            [parse_vector_str(row[0]) for row in cur.fetchall()],
+            dtype=np.float32
+        )
+
+        cur.execute("SELECT a_vector FROM answers")
+        answers_vectors = np.array(
+            [parse_vector_str(row[0]) for row in cur.fetchall()],
+            dtype=np.float32
+        )
+
+    conn.close()
+
+    np.save("questions.npy", questions_vectors)
+    np.save("answers.npy", answers_vectors)
 
 
-# USING MODEL MiniLM
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
-# ENCODING TEXT / IN FUTURE GETTING VECTORS FROM DB
-questions_vectors = model.encode(q, normalize_embeddings=True)
-answers_vectors = model.encode(a, normalize_embeddings=True)
 
 # THIS MODEL IS USING 384 SHAPE TENSOR
-index = faiss.IndexFlatIP(384)  
+index = faiss.IndexFlatL2(384)  
 index.add(answers_vectors)  
 
 # TOP-K SIMILAR ANSWERS
-k = 1 
+k = 5
 
-
-# SCORES TOP-2 EX: [[0.7, O.3],
-#                    [0.12, 0.233]]
-
-# INDICES TOP-2 EX: [[3, 2], 
-#                   [2, 1]]
 
 scores, indices = index.search(questions_vectors, k)
 
-# GETTING INDEX OF QUESTION AND INDEX OF ANSWER TO IT (WORKING FOR TOP-K)
-for q_index, question in enumerate(questions_vectors):
-    print(f"question id: {q_index+1}")
-    for rank in range(k):
-        idx = indices[q_index][rank]          
-        similar = scores[q_index][rank]         
-        print(f"{rank+1}) id: {idx+1} - сходство {similar:.4f} ")
-    print("-" * 30)
+
+# for q_index, question in enumerate(questions_vectors):
+#     print(f"question id: {q_index+1}")
+#     for rank in range(k):
+#         idx = indices[q_index][rank]          
+#         similar = scores[q_index][rank]         
+#         #print(f"{rank+1}) answer_id: {idx+1} - сходство {similar:.4f} ")
+#         data.append((q_index, int(idx)))
+#     print("-" * 30)
 
 
+data = [(q_idx + 1, [int(idx)+1 for idx in indices_row]) 
+        for q_idx, indices_row in enumerate(indices) 
+        ]
+
+
+print(data)
